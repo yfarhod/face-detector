7aff961fbc7cec6240a9f02b6cdb4e989075cff4
yfarhod <49104417+yfarhod@users.noreply.github.com>
2019-12-29 20:24:40 +0100

    url placeholder

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ return (
 		</p>
 		<div className='center'>
 			<div className='form center pa4 br3 shadow-5'>
-				<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+				<input className='f4 pa2 w-70 center' type='text' placeholder='insert image url here' onChange={onInputChange} />
 				<button 
 					className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
 					onClick={onButtonSubmit}
