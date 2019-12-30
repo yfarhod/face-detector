18a05930c35a84d98fbee1318a7da6fd0ce67f09
yfarhod <49104417+yfarhod@users.noreply.github.com>
2019-12-30 21:32:33 +0100

    Updates

---
+++ precache-manifest.fbc09bfecc362db6521e898cf9f2998d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f4d80dc00ac3eaf6b3dcf0021e8d2f",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "29deac8bad8600aa1a72",
+    "url": "/face-detector/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "984b208e17c91ccb8908",
+    "url": "/face-detector/static/css/main.6e67e0e0.chunk.css"
+  },
+  {
+    "revision": "29deac8bad8600aa1a72",
+    "url": "/face-detector/static/js/2.5c95e34c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/face-detector/static/js/2.5c95e34c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "984b208e17c91ccb8908",
+    "url": "/face-detector/static/js/main.9262610e.chunk.js"
+  },
+  {
+    "revision": "d26e68fdb9963e1f5030",
+    "url": "/face-detector/static/js/runtime-main.b929035a.js"
+  }
+]);
